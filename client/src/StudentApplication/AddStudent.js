dff5787b0c17dfa9a9de512701a1b6c17d4b13f0
Jayangi18 <jayangizasso@gmail.com>
2024-08-25 08:15:04 +0530

    RegisteredStudentList added AddStudent Changes

--- client/src/StudentApplication/AddStudent.js
+++ client/src/StudentApplication/AddStudent.js
@@ -59,90 +59,94 @@ function StudentRegistrationForm() {
       {/* Page Title */}
       <PageTitle title="Student Application Form" />
 
-      <div className="mt-3 p-8 rounded-md border  border-gray-200 lg:mx-40 bg-slate-100">
+      <div style={{ padding: "30px", maxWidth: "1000px", margin: "auto" }}>
         <Form
           form={form}
           name="student_registration"
           onFinish={onFinish}
           layout="vertical"
-          style={{ width: '800px', margin: 'auto' }}
+          style={{background: "#f5f5f5",
+            padding: "40px",
+            borderRadius: "10px",
+            position: "relative", 
+            /*width: '800px', margin: 'auto'*/}}
         >
           
-          <Row gutter={150}>
-            <Col span={16}>
-              {/* Personal Information Section */}
-              <Form.Item label="Full Name" name="fullName" rules={[{ required: true, message: 'Please enter your full name' }]}>
-                <Input placeholder="Enter full name" />
-              </Form.Item>
+        <Row gutter={150}>
+          <Col span={16}>
+            {/* Personal Information Section */}
+            <Form.Item label={<b>Full Name</b>} name="fullName" rules={[{ required: true, message: 'Please enter your full name' }]}>
+              <Input placeholder="Enter full name" />
+            </Form.Item>
 
-              <Form.Item label="First Name" name="firstName" rules={[{ required: true, message: 'Please enter your first name' }]}>
-                <Input placeholder="Enter first name" />
-              </Form.Item>
+            <Form.Item label={<b>First Name</b>} name="firstName" rules={[{ required: true, message: 'Please enter your first name' }]}>
+              <Input placeholder="Enter first name" />
+            </Form.Item>
 
-              <Form.Item label="Last Name" name="lastName">
-                <Input placeholder="Enter last name" />
-              </Form.Item>
+            <Form.Item label={<b>Last Name</b>} name="lastName">
+              <Input placeholder="Enter last name" />
+            </Form.Item>
 
-              <Row gutter={30}>
-                <Col span={12}>
-                  <Form.Item label="Date of Birth" name="dob" rules={[{ required: true, message: 'Please select your date of birth' }]}>
+            <Row gutter={30}>
+              <Col span={12}>
+                <Form.Item label={<b>Date of Birth</b>} name="dob" rules={[{ required: true, message: 'Please select your date of birth' }]}>
                     <DatePicker style={{ width: '100%' }} onChange={handleDobChange} />
-                  </Form.Item>
-                </Col>
-                <Col span={12}>
-                  <Form.Item label="Age" name="age">
-                    <Input value={age} readOnly placeholder="Age will be auto-calculated" />
-                  </Form.Item>
-                </Col>
-              </Row>
+                </Form.Item>
+              </Col>
+              <Col span={12}>
+                <Form.Item label={<b>Age</b>} name="age">
+                  <Input value={age} readOnly placeholder="Age will be auto-calculated" />                  
+                </Form.Item>
+              </Col>
+            </Row>
 
-              <Form.Item label="NIC" name="nic"
-                rules={[
-                  {
-                    required: true, 
-                    message: 'Please input your NIC number!',
-                  },
-                  {
-                    validator: validateNic,
-                  },
-                ]}
-              >
-                <Input placeholder="Enter NIC number" />
-              </Form.Item>
+            <Form.Item label="NIC" name="nic"
+              rules={[
+                {
+                  required: true, 
+                  message: 'Please input your NIC number!',
+                },
+                {
+                  validator: validateNic,
+                },
+              ]}
+            >
+            <Input placeholder="Enter NIC number" />
+            </Form.Item>
 
-              <Form.Item label="Gender" name="gender" rules={[{ required: true, message: 'Please select your gender' }]}>
-                <Select placeholder="Select gender">
-                  <Option value="male">Male</Option>
-                  <Option value="female">Female</Option>
-                  {/*<Option value="other">Other</Option>*/}
-                </Select>
-              </Form.Item>
+            <Form.Item label="Gender" name="gender" rules={[{ required: true, message: 'Please select your gender' }]}>
+              <Select placeholder="Select gender">
+                <Option value="male">Male</Option>
+                <Option value="female">Female</Option>
+                {/*<Option value="other">Other</Option>*/}
+              </Select>
+            </Form.Item>
 
-              <Form.Item label="Email" name="email" rules={[{ required: true, type: 'email', message: 'Please enter a valid email' }]}>
-                <Input placeholder="Enter email" />
-              </Form.Item>
+            <Form.Item label="Email" name="email" rules={[{ required: true, type: 'email', message: 'Please enter a valid email' }]}>
+              <Input placeholder="Enter email" />
+            </Form.Item>
 
-              <Form.Item label="Mobile Phone" name="phone" rules={[{ required: true, message: 'Please enter your phone number' }]}>
-                <Input placeholder="Enter mobile phone number" />
-              </Form.Item>
+            <Form.Item label="Mobile Phone" name="phone" rules={[{ required: true, message: 'Please enter your phone number' }]}>
+              <Input placeholder="Enter mobile phone number" />
+            </Form.Item>
 
-              <Form.Item label="Address" name="address">
-                <Input placeholder="Enter address" />
-              </Form.Item>
-            </Col>
+            <Form.Item label="Address" name="address">
+              <Input placeholder="Enter address" />
+            </Form.Item>
+          </Col>
 
-            <div style={{marginTop: '110px'}} >
+          <div style={{marginTop: '110px'}} >
             <Col span={8}>
-              {/* Upload Image Section */}
-              <Form.Item name="upload" valuePropName="fileList">
-                <Upload
-                  name="avatar"
-                  listType="picture-card"
-                  showUploadList={false}
-                  onChange={onUpload}
-                  action="/upload.do"
-                >
-                  <div
+            {/* Upload Image Section */}
+            <Form.Item name="upload" valuePropName="fileList">
+              <Upload
+                name="avatar"
+                listType="picture-card"
+                showUploadList={false}
+                onChange={onUpload}
+                action="/upload.do"
+              >
+                    <div
                     style={{
                       width: '200px',  // Outer square width
                       height: '200px',  // Outer square height
@@ -214,7 +218,16 @@ function StudentRegistrationForm() {
 
           {/* Study Preferences Section */}
           <Form.Item label="Preferred Course/Subject with Specialization" name="preferredCourse">
-            <Input placeholder="Enter preferred course or subject" />
+          <Select placeholder="Enter preferred course or subject">
+            <Option value="s&t">Science and Technology</Option>
+                <Option value="m&h">Medicine and Health</Option>
+                <Option value="b&m">Business and Management</Option>
+                <Option value="e&t">Engineering and Technology</Option>
+                <Option value="art">Art</Option>
+                <Option value="h&ss">Humanities and Social Science</Option>
+                <Option value="law">Law</Option>
+                <option value="education">Education</option>
+            </Select>
           </Form.Item>
 
           <Form.Item label="Preferred Country" name="preferredCountry">
@@ -222,6 +235,7 @@ function StudentRegistrationForm() {
               <Option value="none">None</Option>
               <Option value="australia">Australia</Option>
               <Option value="canada">Canada</Option>
+              <Option value="newZeland">NewZealand</Option>
             </Select>
           </Form.Item>
 
